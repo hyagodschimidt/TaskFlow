ed4515ab12e3b5ffb3dda9805735d6d9f226a4e3
Hyago <Hyago@DESKTOP-7M6PL4A>
2026-08-19 10:01:56 -0300

    Add JWT authentication and token generation

--- server/src/TaskFlow.Api/Program.cs
+++ server/src/TaskFlow.Api/Program.cs
@@ -1,23 +1,49 @@
+using Microsoft.IdentityModel.Tokens;
 using TaskFlow.Application.Interfaces.Security;
 using TaskFlow.Infrastructure.Security;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+var jwtkey = builder.Configuration["Jwt:Key"]
+    ?? throw new InvalidOperationException("JWT key is not configured.");
+
 builder.Services.AddOpenApi();
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(jwtkey)),
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidateIssuer = true,
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            ValidateAudience = true,
+            ValidateLifetime = true,
+        };
+    });
+builder.Services.AddAuthorization();
+builder.Services.Configure<JwtSettings>
+    (builder.Configuration.GetSection("Jwt"));
+
+builder.Services.AddScoped<ITokenGenerator, JwtTokenGenerator>();
 builder.Services.AddScoped<IPasswordHasher, BCryptPasswordHasher>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
 
---
+++ server/src/TaskFlow.Application/Interfaces/Security/ITokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using TaskFlow.Domain.Enums;
+
+namespace TaskFlow.Application.Interfaces.Security
+{
+    public interface ITokenGenerator
+    {
+        string GenerateToken(int userId, int companyId, UserRole role);
+    }
+}
